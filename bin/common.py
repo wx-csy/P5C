352cd55b66d2b5f4aaea445cd3d6e599210d4372
wx-csy <sy_chen@smail.nju.edu.cn>
2019-05-22 14:56:33 +0800

    remove language 'cpp' : C++

--- bin/common.py
+++ bin/common.py
@@ -4,8 +4,8 @@ def die(*args) :
     print(*args, file=sys.stderr)
     exit(1)
 
-def commit(msg, path=None) :
-    if path and subprocess.call(['git', 'add', '--verbose', path]) != 0 :
+def commit(msg, path='.') :
+    if subprocess.call(['git', 'add', '--verbose', path]) != 0 :
         die("git: failed to add changes")
     if subprocess.call(['git', 'commit', '-am', msg]) != 0 :
         die("git: failed to commit changes")
