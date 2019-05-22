35ac429e73b952591c297f91f7bec817a0516a65
wx-csy <sy_chen@smail.nju.edu.cn>
2019-05-22 15:47:05 +0800

    remove language 'cpp'

--- bin/lang.py
+++ bin/lang.py
@@ -5,6 +5,7 @@ def add_lang(shortname) :
     com.checkparam(shortname)
     name = com.readparam('Full Name: ', default=shortname)
     ext = com.readparam('File Extension (space-separated): ', default='').split()
+    if not ext : die("extension list must not be empty")
     meta = com.load_meta(default=dict())
     if shortname in meta :
         com.die("language '{0}' already exists!".format(shortname))
@@ -16,6 +17,7 @@ def add_lang(shortname) :
     shutil.copytree('../resource/lang-template', shortname)
     subprocess.check_call([com.config['editor'], shortname + '/compile'])
     subprocess.check_call([com.config['editor'], shortname + '/run'])
+    subprocess.check_call([com.config['editor'], shortname + '/template'])
     com.commit("add language '{0}' : {1}".format(shortname, name));
 
 def remove_lang(shortname) :
